fc4fa566593b396a91d6b8c55d38b1dab602c8f3
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-27 00:57:50 +0530

    cta

--- components/sections/CTA.tsx
+++ components/sections/CTA.tsx
@@ -4,17 +4,32 @@ const CTA = () => {
   return (
     <section className="relative py-20 px-4 mt-20 mb-24 flex flex-col items-center justify-center max-w-7xl mx-auto rounded-2xl shadow-md gap-12">
       
-      <div className="absolute inset-0 w-full bg-grid pointer-events-none rounded-2xl" aria-hidden="true"  style={{
-        // horizontal fade on edges using mask-image
-        WebkitMaskImage: 'linear-gradient(to right, transparent 0%, black 40%, black 60%, transparent 100%)',
-      maskImage: 'linear-gradient(to right, transparent 0%, black 40%, black 60%, transparent 100%)',
-      }} />
-      
      
+      <div
+        className="absolute inset-0 w-full bg-grid pointer-events-none rounded-2xl"
+        aria-hidden="true"
+        style={{
+          WebkitMaskImage:
+            "linear-gradient(to right, transparent 0%, black 40%, black 60%, transparent 100%)",
+          maskImage:
+            "linear-gradient(to right, transparent 0%, black 40%, black 60%, transparent 100%)",
+        }}
+      />
+
+      
       <div className="absolute inset-0 w-full h-full bg-black rounded-2xl opacity-90" aria-hidden="true" />
-      
-      
-      <div className="relative flex flex-col items-center justify-center gap-2">
+
+      <div
+        className="absolute top-[300px] left-1/2 w-[600px] h-[600px] -translate-x-1/2 -translate-y-1/2 rounded-full pointer-events-none z-0"
+        aria-hidden="true"
+        style={{
+          background: "radial-gradient(circle, rgba(200,200,200,0.2) 0%, transparent 70%)",
+          filter: "blur(10px)",
+        }}
+      />
+
+   
+      <div className="relative z-10 flex flex-col items-center justify-center gap-2">
         <h1 className="md:text-6xl text-4xl pt-20 font-khula font-extrabold text-white tracking-tighter items-center">
           Your All-in-one Learning
         </h1>
--- components/sections/Testimonials.tsx
+++ components/sections/Testimonials.tsx
@@ -46,7 +46,7 @@ const testimonials = [
 
 export default function TestimonialsSection() {
   return (
-    <section className="py-12 px-4 flex flex-col items-center justify-center mx-4 bg-black rounded-2xl shadow-md gap-12">
+    <section className="py-12 px-4 flex flex-col items-center justify-center mx-4 bg-black rounded-2xl shadow-md gap-20">
       <div className="flex flex-col items-center justify-center gap-2">
       <h2 className="text-4xl px-16 py-4 font-khula font-semibold text-white tracking-tighter border-1 border-white" style={{ borderWidth: '0.5px' }}>
   Loved by people everywhere
