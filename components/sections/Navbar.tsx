381c6b0629240623106fa0f531ebf62d0e1c9346
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-26 23:39:12 +0530

    logo

--- components/sections/Navbar.tsx
+++ components/sections/Navbar.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Link from "next/link";
+import Icon from "@/app/favicon.svg"
 
 const Navbar = () => {
   return (
     <nav className="sticky bg-slate-950 rounded-full top-4 right-0 z-50 max-w-7xl mx-auto py-4 px-10 flex justify-between items-center">
       <Link href="/">
-        <h1 className="text-2xl font-extrabold text-white tracking-tighter">
-          MeshSpire
-        </h1>
+        <img
+          src="/MeshSpire-Dark 1.svg"
+          alt="Dashboard dark"
+          width={140}
+          height={100}
+        />
+
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       </Link>
       <div className="flex items-center font-catamaran gap-32">
         <Link
@@ -32,12 +38,12 @@ const Navbar = () => {
           href="#contact"
           className="text-slate-400 font-medium tracking-tight hover:text-white transition-all duration-300"
         >
-         Contact us
+          Contact us
         </Link>
-       
+
       </div>
       <div>
-      <button className="bg-green-600 font-catamaran text-white font-bold px-10 py-2 hover:bg-green-900 transition-all duration-300 rounded-full">
+        <button className="bg-green-600 font-catamaran text-white font-bold px-10 py-2 hover:bg-green-900 transition-all duration-300 rounded-full">
           Login
         </button>
       </div>
